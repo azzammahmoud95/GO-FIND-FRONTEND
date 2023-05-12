640efa51856830cca0fca14b338e3ca5bb5161a0
Azzam Mahmoud <azzammahmoud007@gmail.com>
2023-05-12 14:44:00 +0300

    login page before mui

--- gofind-frontend/src/pages/Signup/Signup.js
+++ gofind-frontend/src/pages/Signup/Signup.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import LogonameBlackGreen from "../.././assests/Elements/LogonameBlackGreen.svg"
 import styles from "./Signup.module.css"
+import DirBordGreen1 from "../../assests/Elements/DirBordGreen1.svg"
+import k from "../../assests/Elements/MagniBordGreen.svg"
 export default function Signup() {
   return (
-    <div>
+    <div className={styles.loginPage}>
         <div className={styles.topSide}>
-        <img src={LogonameBlackGreen} alt="logo"  width={'150'}/>
+        <img src={LogonameBlackGreen} alt="logo"  width={'200'} className={styles.logoStyle}/>
+        </div>
+        <div className={styles.CenterSide}>
+            <form action="" className={styles.formStyle}>
+                <h2>Signin </h2>
+            </form>
         </div>
     </div>
   )
